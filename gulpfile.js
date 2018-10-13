aff584c239e14b1e82a2a7b557b754bc4a9c8b81
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-10-13 13:39:23 +0200

    Image minification task added

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const uglifyCss = require('gulp-uglifycss');
 const sourceMaps = require('gulp-sourcemaps');
+const imageMin = require('gulp-imagemin');
 
+//This task initializes the js sourcemaps and concatenates the js files
 gulp.task('concatScripts', () => {
     return gulp.src(['js/*.js', 'js/circle/*.js'])
                .pipe(sourceMaps.init())
@@ -14,6 +16,7 @@ gulp.task('concatScripts', () => {
                .pipe(gulp.dest('js'))
 })
 
+//Minifies the js files
 gulp.task('scripts', ['concatScripts'], () => {
      return gulp.src('js/all.js')
                 .pipe(uglify())
@@ -21,6 +24,7 @@ gulp.task('scripts', ['concatScripts'], () => {
                 .pipe(gulp.dest('dist/scripts'))
 })
 
+//Initializes css sourcemaps and compiles the sass to css
 gulp.task('compileSass', () => {
     return gulp.src('sass/global.scss')
                .pipe(sourceMaps.init())
@@ -30,10 +34,21 @@ gulp.task('compileSass', () => {
                .pipe(gulp.dest('css'))
 })
 
+//Minifies the css files
 gulp.task('styles', ['compileSass'], () => {
     return gulp.src('css/all.css')
                .pipe(uglifyCss())
                .pipe(rename('all.min.css'))
                .pipe(gulp.dest('dist/styles'))
 })
+
+//Minifies the image files
+
+gulp.task('minifyImages', () => {
+    return gulp.src('images/*')
+               .pipe(imageMin())
+               .pipe(gulp.dest('dist/images'))
+})
+
+
 
