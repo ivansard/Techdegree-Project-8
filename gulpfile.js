f12fe87a1789f9ae296d442c340b1c14e4a07a59
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-10-13 13:34:27 +0200

    CSS and JS sourcemaps generated

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const uglifyCss = require('gulp-uglifycss');
+const sourceMaps = require('gulp-sourcemaps');
 
 gulp.task('concatScripts', () => {
     return gulp.src(['js/*.js', 'js/circle/*.js'])
+               .pipe(sourceMaps.init())
                .pipe(concat('all.js'))
+               .pipe(sourceMaps.write('../dist/scripts'))
                .pipe(gulp.dest('js'))
 })
 
@@ -20,8 +23,10 @@ gulp.task('scripts', ['concatScripts'], () => {
 
 gulp.task('compileSass', () => {
     return gulp.src('sass/global.scss')
+               .pipe(sourceMaps.init())
                .pipe(sass())
                .pipe(rename('all.css'))
+               .pipe(sourceMaps.write('../dist/styles'))
                .pipe(gulp.dest('css'))
 })
 
@@ -31,3 +36,4 @@ gulp.task('styles', ['compileSass'], () => {
                .pipe(rename('all.min.css'))
                .pipe(gulp.dest('dist/styles'))
 })
+
