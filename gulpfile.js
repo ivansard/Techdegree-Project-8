d94cb1c6aa1f00fb9086dd83b034ad32d7072a1e
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-10-17 10:43:05 +0200

    Default task added, serving from server successful

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,48 @@ const sourceMaps = require('gulp-sourcemaps');
 const imageMin = require('gulp-imagemin');
 const del = require('del');
 const runSequence = require('run-sequence');
-const webserver = require('gulp-webserver');
+const browserSync = require('browser-sync').create();
+
+const options = {
+    app: 'app',
+    dist: 'dist'
+}
 
 //This task initializes the js sourcemaps and concatenates the js files
 gulp.task('concatScripts', () => {
-    return gulp.src('js/circle/*.js')
+    return gulp.src(options.app + '/js/circle/*.js')
                .pipe(sourceMaps.init())
                .pipe(concat('global.js'))
-               .pipe(sourceMaps.write('../dist/scripts'))
-               .pipe(gulp.dest('js'))
+               .pipe(sourceMaps.write('../js'))
+               .pipe(gulp.dest(options.app + '/js'))
 })
 
 //Minifies the js files
 gulp.task('scripts', ['concatScripts'], () => {
-     return gulp.src('js/global.js')
+     return gulp.src(options.app + '/js/global.js')
                 .pipe(uglify())
                 .pipe(rename('all.min.js'))
                 .pipe(gulp.dest('dist/scripts'))
 })
 
+//MORAJU MAPE U DIST!!!
+
 //Initializes css sourcemaps and compiles the sass to css
 gulp.task('compileSass', () => {
-    return gulp.src('sass/global.scss')
+    return gulp.src(options.app + '/sass/global.scss')
                .pipe(sourceMaps.init())
                .pipe(sass())
                .pipe(rename('global.css'))
-               .pipe(sourceMaps.write('../dist/styles'))
-               .pipe(gulp.dest('css'))
+               .pipe(sourceMaps.write('../css'))
+               .pipe(gulp.dest(options.app + '/css'))
+               .pipe(browserSync.reload({
+                   stream: true
+               }))
 })
 
 //Minifies the css files
 gulp.task('styles', ['compileSass'], () => {
-    return gulp.src('css/global.css')
+    return gulp.src(options.app + '/css/global.css')
                .pipe(uglifyCss())
                .pipe(rename('all.min.css'))
                .pipe(gulp.dest('dist/styles'))
@@ -47,37 +57,40 @@ gulp.task('styles', ['compileSass'], () => {
 
 //Minifies the image files
 gulp.task('images', () => {
-    return gulp.src(['images/*', 'icons/*'])
+    return gulp.src([options.app + '/images/*',options.app + '/icons/*'])
                .pipe(imageMin())
                .pipe(gulp.dest('dist/content'))
 })
 
+//Watches for changes in scss files
+gulp.task('watch', () => {
+    gulp.watch('app/sass/**/*.scss', ['styles'])
+})
+
 //Deletes the dist folder
 gulp.task('clean', () => {
-    return del('dist/**')
+    return del.sync('dist/**', '!../dist');
 })
 
 //Generates all the needed content in the dist folder - css, js, images
 gulp.task('generateDistContent', ['scripts', 'styles', 'images'], () => {
-    return gulp.src(['index.html', 'icons/**'], {base: './'})
+    return gulp.src('app/index.html')
                .pipe(gulp.dest('dist'));
 });
 
 //First executes the clean task, and then the generateDistContent task
 gulp.task('build', () => {
     return runSequence('clean'
-                , 'generateDistContent');
+                , 'generateDistContent', 'watch');
 })
 
+//Starts web-server
 gulp.task('startServer', () => {
-    return gulp.src('../dist')
-        .pipe(webserver({
-            livereload: true,
-            host: 'localhost',
-            port: 3000,
-            open: true,
-            fallback: 'index.html'
-        }))
+    return browserSync.init({
+        server: {
+          baseDir: 'app'
+        },
+      })
 })
 
 //Default task, which additionally creates a web-server
